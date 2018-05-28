dd8483fa716c49e030d394340c260bdd88858e5f
alice <alice.guercio@atcult.it>
2018-05-28 15:00:54 +0200

    start commit

---
+++ test/ui-testing/demo.js
@@ -0,0 +1,49 @@
+/* global Nightmare, describe, it, before, after */
+
+module.exports.test = (uiTestCtx) => {
+  describe('Module test: ui-cataloging:', function() {
+    const { config, helpers: { login, logout } } = uiTestCtx;
+    const nightmare = new Nightmare(config.nightmare);
+
+    this.timeout(Number(config.test_timeout));
+
+    describe('Login > navigate to app > verify message > logout', () => {
+      before((done) => {
+        login(nightmare, config, done);
+      });
+      after((done) => {
+        logout(nightmare, config, done);
+      });
+      it('should open app and see stripes-new-app-greeting', (done) => {
+        nightmare
+          .wait('#clickable-cataloging-module')
+          .click('#clickable-cataloging-module')
+          .wait('#cataloging-module-display')
+          .wait('#stripes-new-app-greeting')
+          .then(result => { done(); })
+          .catch(done);
+      });
+    });
+
+    describe('Login > navigate to app settings > verify message > logout', () => {
+      before((done) => {
+        login(nightmare, config, done);
+      });
+      after((done) => {
+        logout(nightmare, config, done);
+      });
+      it('should open app settings and see stripes-new-app-settings-message', (done) => {
+        nightmare
+          .wait(config.select.settings)
+          .click(config.select.settings)
+          .wait('a[href="/settings/cataloging"]')
+          .click('a[href="/settings/cataloging"]')
+          .wait('a[href="/settings/cataloging/general"]')
+          .click('a[href="/settings/cataloging/general"]')
+          .wait('#stripes-new-app-settings-message')
+          .then(result => { done(); })
+          .catch(done);
+      });
+    });
+  });
+};

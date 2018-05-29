7c2a08eca161e7200f54f4d73f0ce7ffdc443391
Unknown <christian.chiama@atcult.it>
2018-05-29 13:05:57 +0200

    update structure
    update structure

--- src/index.js
+++ src/index.js
@@ -3,18 +3,19 @@ import PropTypes from 'prop-types';
 import Switch from 'react-router-dom/Switch';
 import Route from 'react-router-dom/Route';
 import Application from './routes/application';
+import Settings from './settings';
 
-/*
-  STRIPES-NEW-APP
-  This is the main entry point into your new app.
-*/
 
 class Cataloging extends React.Component {
   static propTypes = {
-    match: PropTypes.object.isRequired
+    match: PropTypes.object.isRequired,
+    showSettings: PropTypes.bool,
   }
 
   render() {
+    if (this.props.showSettings) {
+      return <Settings {...this.props} />;
+    }
     return (
       <Switch>
         <Route path={`${this.props.match.path}`} exact component={Application} />

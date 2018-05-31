e08a626b8566f3705da7d8bc7afd49068773b461
Christian Chiama <christian.chiama@atcult.it>
2018-05-31 20:49:01 +0200

    update

--- src/Root/Root.js
+++ src/Root/Root.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 
 class Root extends Component {
   render() {
@@ -8,9 +7,5 @@ class Root extends Component {
     );
   }
 }
-
-Root.propTypes = {
-
-};
 
 export default Root;

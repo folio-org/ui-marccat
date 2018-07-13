703be8faa320b1fd1eea5a1a7208a9ec0289d98a
cchiama <christian.chiama@atcult.it>
2018-07-13 09:33:09 +0200

    refatcoring with type flow

--- src/App/MARCcat.js
+++ src/App/MARCcat.js
@@ -2,31 +2,17 @@
  * @format
  * @flow
  */
-import PropTypes from 'prop-types'; // eslint-disable-line no-unused-vars
-import React from 'react';
+import * as React from 'react';
 import NavDrawer from '../Navigator/components/NavDrawer';
 
-export default class MARCcat extends React.Component<PropTypes> {
-  static propTypes = {
-    stripes: PropTypes.shape({
-      intl: PropTypes.object.isRequired,
-      locale: PropTypes.string.isRequired,
-      connect: PropTypes.func,
-      store: PropTypes.object,
-    }).isRequired,
-    resources: PropTypes.shape({
-      // eslint-disable-line no-unused-vars
-    }).isRequired,
-    mutator: PropTypes.shape({
-      // eslint-disable-line no-unused-vars
-      initializedFilterConfig: PropTypes.shape({
-        replace: PropTypes.func.isRequired,
-      }),
-      records: PropTypes.shape({
-        POST: PropTypes.func.isRequired,
-      }),
-    }).isRequired,
-  };
+type Props = {
+  stripes: Object,
+  resources: Object,
+  mutator: Object,
+};
+
+
+export default class MARCcat extends React.Component<Props, {}> {
 
   static manifest = Object.freeze({
     query: { initialValue: {} },

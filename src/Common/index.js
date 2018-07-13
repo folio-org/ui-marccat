f9f505065ee72c6c010c3756dbd68d5f3f0156e7
cchiama <christian.chiama@atcult.it>
2018-07-13 09:53:33 +0200

    remove files

--- src/Common/index.js
+++ src/Common/index.js
@@ -3,6 +3,5 @@ export { default as Select } from './FormField/Select';
 export { default as SnackBar } from './Message/SnackBar';
 export { default as SimpleSelect } from './FormField/SimpleSelect';
 export { default as VersionModal } from './Modal/VersionModal';
-export { default as FabSaveProgress } from './Button/FabSaveProgress';
 
 export { CSS3Slide, CSS3Fade } from './Anim/Transition';

03371723b0480172ffa31eb1772f27b0a46baeb7
Matteo Pascucci <matteo.pascucci@atcult.it>
2018-07-17 14:48:54 +0200

    Merge from FMM1

--- src/Index/IndexList.js
+++ src/Index/IndexList.js
@@ -1,21 +1,33 @@
 import React from 'react';
 import Pane from '@folio/stripes-components/lib/Pane';
 import Paneset from '@folio/stripes-components/lib/Paneset';
+import { FormattedMessage } from 'react-intl';
+import Button from '@folio/stripes-components/lib/Button';
 import { connect } from '@folio/stripes-connect';
 import * as C from '../Utils';
+import s from '../Theme/override.css';
 
 type IndexListProps = {
 };
 
 type IndexListState = {
   open: boolean;
+const mainIndexResults = require('../../config/static/main-index-list')
+const secondaryIndexResults = require('../../config/static/secondary-index-list')
+
+function printDiv(divName, anotherDivName) {
+  let printContentsMain = document.getElementById(divName).innerHTML;
+  let printContentsSecondary = document.getElementById(anotherDivName).innerHTML;
+  let originalContents = document.body.innerHTML;
+  document.body.innerHTML = "--------------------------------------------------------------- MAIN INDEXES ---------------------------------------------------------------" + printContentsMain + "---------------------------------------------------------- SECONDARY INDEXES ----------------------------------------------------------" + printContentsSecondary;
+  window.print();
+  document.body.innerHTML = originalContents;
 }
 
 class IndexList extends React.Component<IndexListProps, IndexListState> {
   constructor(props) {
     super(props);
     this.state = {
-      open: true, // eslint-disable-line react/no-unused-state
     };
   }
 
@@ -25,28 +37,101 @@ class IndexList extends React.Component<IndexListProps, IndexListState> {
         static
       >
         <Pane
-          dismissible
           defaultWidth="100%"
-          paneTitle={<h2>INDEX GUIDE</h2>}
+          paneTitle='INDEX GUIDE'
         >
+          <Button
+            onClick={() => printDiv('main', 'secondary')}
+            type="button"
+            buttonStyle="primary"
+            style={{position: 'absolute' ,minHeight: '36px' }}
+          >
+            <FormattedMessage id="Print" />
+          </Button>
           <Paneset static>
             <Pane
               defaultWidth="fill"
-              paneTitle={<h3>Main Indexes</h3>}
-              paneSub=""
+              paneTitle='Main Indexes'
               appIcon={{ app: 'cataloging' }}
-            />
+            >
+              <div id='main'>
+                {
+                  mainIndexResults.map((dynamicData) =>
+                    <table style={{ float: 'left', width: '50%', paddingTop: '50pt' }}>
+                      <thead>
+                        <tr>
+                          <th colSpan="2"> <p><h2 className={s.CustomIndexesH2}>{dynamicData.title}</h2></p></th>
+                        </tr>
+                        {
+                          dynamicData.descriptions.map((dynamicValues) =>
+                            <tbody>
+                              <tr className={s.CustomIndexTR}>
+                                <td colSpan="1"><b>{dynamicValues.key}</b></td>
+                                <td colSpan="1">{dynamicValues.value}</td>
+                              </tr>
+                            </tbody>
+                          )
+                        }
+                      </thead>
+                    </table>
+                  )
+                }
+                <div >
+                  <h1 style={{ visibility: 'hidden' }}>MAIN INDEXES</h1>
+                </div>
+                <div >
+                  <h1 style={{ visibility: 'hidden' }}>MAIN INDEXES</h1>
+                </div>
+                <div >
+                  <h1 style={{ visibility: 'hidden' }}>MAIN INDEXES</h1>
+                </div>
+                <div >
+                  <h1 style={{ visibility: 'hidden' }}>MAIN INDEXES</h1>
+                </div>
+                <div >
+                  <h1 style={{ visibility: 'hidden' }}>MAIN INDEXES</h1>
+                </div>
+                <div >
+                  <h1 style={{ visibility: 'hidden' }}>MAIN INDEXES</h1>
+                </div>
+                <div >
+                  <h1 style={{ visibility: 'hidden' }}>MAIN INDEXES</h1>
+                </div>
+              </div>
+            </Pane>
             <Pane
               defaultWidth="fill"
-              paneTitle={<h3>Secondary Indexes</h3>}
-              paneSub=""
+              paneTitle='Secondary Indexes'
               appIcon={{ app: 'cataloging' }}
-            />
+            >
+              <div id='secondary'>
+                {
+                  secondaryIndexResults.map((dynamicData) =>
+                    <table style={{ float: 'left', width: '50%', paddingTop: '50pt' }}>
+                      <thead>
+                        <tr>
+                          <th colSpan="2"> <p><h2 className={s.CustomIndexesH2}>{dynamicData.title}</h2></p></th>
+                        </tr>
+                        {
+                          dynamicData.descriptions.map((dynamicValues) =>
+                            <tbody>
+                              <tr className={s.CustomIndexTR}>
+                                <td colSpan="1"><b>{dynamicValues.key}</b></td>
+                                <td colSpan="1">{dynamicValues.value}</td>
+                              </tr>
+                            </tbody>
+                          )
+                        }
+                      </thead>
+                    </table>
+                  )
+                }
+              </div>
+            </Pane>
           </Paneset>
         </Pane>
       </Paneset>
     );
   }
 }
-
-export default connect(IndexList, C.META.MODULE_NAME);
+export default connect(IndexList, C.META.MODULE_NAME)

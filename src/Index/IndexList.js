86b672bd8210c17228eb089f004638dcb0f81239
Matteo Pascucci <matteo.pascucci@atcult.it>
2018-07-19 11:05:27 +0200

    fix IndexList.js

--- src/Index/IndexList.js
+++ src/Index/IndexList.js
@@ -1,32 +1,26 @@
 
-import React from 'react';
+import Button from '@folio/stripes-components/lib/Button';
 import Pane from '@folio/stripes-components/lib/Pane';
 import Paneset from '@folio/stripes-components/lib/Paneset';
-import { FormattedMessage } from 'react-intl';
-import Button from '@folio/stripes-components/lib/Button';
 import { connect } from '@folio/stripes-connect';
-import * as C from '../Utils';
+import React from 'react';
+import { FormattedMessage } from 'react-intl';
 import s from '../Theme/override.css';
+import * as C from '../Utils';
 
-type IndexListProps = {
-};
-
-type IndexListState = {
-  open: boolean;
-}
 const mainIndexResults = require('../../config/static/main-index-list');
 const secondaryIndexResults = require('../../config/static/secondary-index-list');
 
 function printDiv(divName, anotherDivName) {
-  const printContentsMain = document.getElementById(divName).textContent;// eslint-disable-lines
-  const printContentsSecondary = document.getElementById(anotherDivName).textContent;
-  const originalContents = document.body.textContent;
-  document.body.textContent = `--------------------------------------------------------------- MAIN INDEXES ---------------------------------------------------------------" + printContentsMain + "---------------------------------------------------------- SECONDARY INDEXES ----------------------------------------------------------${printContentsSecondary}`;
+  const printContentsMain = document.getElementById(divName).innerHTML;
+  const printContentsSecondary = document.getElementById(anotherDivName).innerHTML;
+  const originalContents = document.body.innerHTML;
+  document.body.innerHTML = `--------------------------------------------------------------- MAIN INDEXES ---------------------------------------------------------------${printContentsMain}---------------------------------------------------------- SECONDARY INDEXES ----------------------------------------------------------${printContentsSecondary}`;
   window.print();
-  document.body.textContent = originalContents;
+  document.body.innerHTML = originalContents;
 }
 
-class IndexList extends React.Component<IndexListProps, IndexListState> {
+class IndexList extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -38,18 +32,18 @@ class IndexList extends React.Component<IndexListProps, IndexListState> {
       <Paneset
         static
       >
+        <Button
+          onClick={() => printDiv('main', 'secondary')}
+          type="button"
+          buttonStyle="primary"
+          style={{ position: 'absolute', minHeight: '36px' }}
+        >
+          <FormattedMessage id="Print" />
+        </Button>
         <Pane
           defaultWidth="100%"
           paneTitle="INDEX GUIDE"
         >
-          <Button
-            onClick={() => printDiv('main', 'secondary')}
-            type="button"
-            buttonStyle="primary"
-            style={{ position: 'absolute', minHeight: '36px' }}
-          >
-            <FormattedMessage id="Print" />
-          </Button>
           <Paneset static>
             <Pane
               defaultWidth="fill"
@@ -58,43 +52,45 @@ class IndexList extends React.Component<IndexListProps, IndexListState> {
             >
               <div id="main">
                 {
-                  mainIndexResults.map((dynamicData) =>
-                    (<table style={{ float: 'left', width: '50%', paddingTop: '50pt' }}>
+                  mainIndexResults.map((dynamicMainData) => (
+                    <table style={{ float: 'left', width: '50%', paddingTop: '30pt' }}>
                       <thead>
                         <tr>
-                          <th colSpan="2"> <p><h2 className={s.CustomIndexesH2}>{dynamicData.title}</h2></p></th>
+                          <th colSpan="2"> <p><h2 className={s.CustomIndexesH2}>{dynamicMainData.title}</h2></p></th>
                         </tr>
-                        {
-                          dynamicData.descriptions.map((dynamicValues) =>
-                            (<tbody>
-                              <tr className={s.CustomIndexTR}>
-                                <td colSpan="1"><b>{dynamicValues.key}</b></td>
-                                <td colSpan="1">{dynamicValues.value}</td>
-                              </tr>
-                            </tbody>)
-                          )}
                       </thead>
-                    </table>)
-                } 
-                <div>
+                      {
+                        dynamicMainData.descriptions.map((dynamicMainValues) => (
+                          <tbody>
+                            <tr className={s.CustomIndexTR}>
+                              <td colSpan="1"><b>{dynamicMainValues.key}</b></td>
+                              <td colSpan="1">{dynamicMainValues.value}</td>
+                            </tr>
+                          </tbody>
+                        ))
+                      }
+                    </table>
+                  ))
+                }
+                <div >
                   <h1 style={{ visibility: 'hidden' }}>MAIN INDEXES</h1>
                 </div>
-                <div>
+                <div >
                   <h1 style={{ visibility: 'hidden' }}>MAIN INDEXES</h1>
                 </div>
-                <div>
+                <div >
                   <h1 style={{ visibility: 'hidden' }}>MAIN INDEXES</h1>
                 </div>
-                <div>
+                <div >
                   <h1 style={{ visibility: 'hidden' }}>MAIN INDEXES</h1>
                 </div>
-                <div>
+                <div >
                   <h1 style={{ visibility: 'hidden' }}>MAIN INDEXES</h1>
                 </div>
-                <div>
+                <div >
                   <h1 style={{ visibility: 'hidden' }}>MAIN INDEXES</h1>
                 </div>
-                <div>
+                <div >
                   <h1 style={{ visibility: 'hidden' }}>MAIN INDEXES</h1>
                 </div>
               </div>
@@ -106,23 +102,25 @@ class IndexList extends React.Component<IndexListProps, IndexListState> {
             >
               <div id="secondary">
                 {
-                  secondaryIndexResults.map((dynamicData) =>
-                    (<table style={{ float: 'left', width: '50%', paddingTop: '50pt' }}>
+                  secondaryIndexResults.map((dynamicSecondaryData) => (
+                    <table style={{ float: 'left', width: '50%', paddingTop: '30pt' }}>
                       <thead>
                         <tr>
-                          <th colSpan="2"> <p><h2 className={s.CustomIndexesH2}>{dynamicData.title}</h2></p></th>
+                          <th colSpan="2"> <p><h2 className={s.CustomIndexesH2}>{dynamicSecondaryData.title}</h2></p></th>
                         </tr>
-                        {
-                          dynamicData.descriptions.map((dynamicValues) =>
-                            (<tbody>
-                              <tr className={s.CustomIndexTR}>
-                                <td colSpan="1"><b>{dynamicValues.key}</b></td>
-                                <td colSpan="1">{dynamicValues.value}</td>
-                              </tr>
-                            </tbody>))
-                        }
                       </thead>
-                    </table>))
+                      {
+                        dynamicSecondaryData.descriptions.map((dynamicMainValues) => (
+                          <tbody>
+                            <tr className={s.CustomIndexTR}>
+                              <td colSpan="1"><b>{dynamicMainValues.key}</b></td>
+                              <td colSpan="1">{dynamicMainValues.value}</td>
+                            </tr>
+                          </tbody>
+                        ))
+                      }
+                    </table>
+                  ))
                 }
               </div>
             </Pane>

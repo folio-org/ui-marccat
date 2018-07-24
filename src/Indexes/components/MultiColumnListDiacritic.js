4b269541b094dbad36a816d4238272a9b502ccb2
Matteo Pascucci <matteo.pascucci@atcult.it>
2018-07-24 16:03:00 +0200

    Add Diacritics and Index List fixed

--- src/Indexes/components/MultiColumnListDiacritic.js
+++ src/Indexes/components/MultiColumnListDiacritic.js
@@ -5,63 +5,65 @@ import { connect } from '@folio/stripes-connect';
 import * as C from '../../Utils';
 
 type MultiColumnListDiacriticProps = {
-    stripes: Object;
-    resources:Object;
+  stripes: Object;
+  resources: Object;
+  onRowClick: Function,
 };
 
 type MultiColumnListDiacriticState = {
-    value: string;
-    charCopied: string;
-    isOpen: boolean;
+  value: string;
+  charCopied: string;
+  isOpen: boolean;
 };
 
 class MultiColumnListDiacritic extends Component
-<MultiColumnListDiacriticProps, MultiColumnListDiacriticState> {
-    static manifest = Object.freeze({
-      views: {
-        type: C.RESOURCE_TYPE,
-        root: C.ENDPOINT.BASE_URL,
-        path: C.ENDPOINT.DIACRITIC_LIST_URL,
-        headers: C.ENDPOINT.HEADERS,
-        records: C.API_RESULT_JSON_KEY.DIACRITIC,
-        GET: {
-          params: { lang: C.ENDPOINT.DEFAULT_LANG },
-        },
+  <MultiColumnListDiacriticProps, MultiColumnListDiacriticState> {
+  static manifest = Object.freeze({
+    views: {
+      type: C.RESOURCE_TYPE,
+      root: C.ENDPOINT.BASE_URL,
+      path: C.ENDPOINT.DIACRITIC_LIST_URL,
+      headers: C.ENDPOINT.HEADERS,
+      records: C.API_RESULT_JSON_KEY.DIACRITIC,
+      GET: {
+        params: { lang: C.ENDPOINT.DEFAULT_LANG },
       },
-    });
+    },
+  });
 
-    constructor(props: MultiColumnListDiacriticProps) {
-      super(props);
-      this.state = {
-      };
-      /** bind handler **/
-    }
+  constructor(props: MultiColumnListDiacriticProps) {
+    super(props);
+    this.state = {
+      charCopied: '',//eslint-disable-line
+    };
+    /** bind handler **/
+  }
 
-
-    render() {
-      const { resources: { views } } = this.props;
-      if (!views || !views.hasLoaded) return <Icon icon="spinner-ellipsis" />;
-      const data = views.records;
-      const formatMsg = this.props.stripes.intl.formatMessage;
-      const columnMapping = {
-        value: formatMsg({ id: 'ui-marccat.diacritic.list.id' }),
-        character: formatMsg({ id: 'ui-marccat.diacritic.list.character' }),
-        label: formatMsg({ id: 'ui-marccat.diacritic.list.description' }),
-        characterSet: formatMsg({ id: 'ui-marccat.diacritic.list.charset' }),
-        unicode: formatMsg({ id: 'ui-marccat.diacritic.list.unicode' }),
-      };
-      return (
-        <MultiColumnList
-          contentData={data}
-          visibleColumns={[
-            'value',
-            'character',
-            'label',
-            'characterSet',
-            'unicode']}
-          columnMapping={columnMapping}
-        />
-      );
-    }
+  render() {
+    const { resources: { views } } = this.props;
+    if (!views || !views.hasLoaded) return <Icon icon="spinner-ellipsis" />;
+    const data = views.records;
+    const formatMsg = this.props.stripes.intl.formatMessage;
+    const columnMapping = {
+      value: formatMsg({ id: 'ui-marccat.diacritic.list.id' }),
+      character: formatMsg({ id: 'ui-marccat.diacritic.list.character' }),
+      label: formatMsg({ id: 'ui-marccat.diacritic.list.description' }),
+      characterSet: formatMsg({ id: 'ui-marccat.diacritic.list.charset' }),
+      unicode: formatMsg({ id: 'ui-marccat.diacritic.list.unicode' }),
+    };
+    return (
+      <MultiColumnList
+        onRowClick={this.props.onRowClick}
+        contentData={data}
+        visibleColumns={[
+          'value',
+          'character',
+          'label',
+          'characterSet',
+          'unicode']}
+        columnMapping={columnMapping}
+      />
+    );
+  }
 }
 export default connect(MultiColumnListDiacritic, C.META.MODULE_NAME);

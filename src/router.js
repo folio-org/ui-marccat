e3d88ce96ae0a5a647407bd19892945ef059f39b
Christian Chiama <christian.chiama@atcult.it>
2018-07-26 16:04:14 +0200

    fix navigator empty
    fix navigator empty

--- src/router.js
+++ src/router.js
@@ -5,11 +5,11 @@
 import React from 'react';
 import Route from 'react-router-dom/Route';
 import Switch from 'react-router-dom/Switch';
+import Paneset from '@folio/stripes-components/lib/Paneset';
+import Pane from '@folio/stripes-components/lib/Pane';
 import { TemplateView, CreateTemplate } from './Template/';
 import { SimpleSearch, SearchResults } from './Search/';
 import { IndexList, Diacritic } from './Indexes/';
-import MARCCat from './App/MARCCat';
-
 
 export function ConnectedRoute({ path, id, component: Component, ...props }) { // eslint-disable-line react/prop-types
   return (
@@ -19,13 +19,24 @@ export function ConnectedRoute({ path, id, component: Component, ...props }) { /
   );
 }
 
+const Empty = () => {
+  return(
+  <Paneset static>
+    <Pane
+      defaultWidth="fill"
+      paneTitle="Marccat"
+      paneSub=""
+      onClose={() => {}}
+      />
+  </Paneset>);
+}
+
 
 export default class Router extends React.Component<*> {
   render() {
     const rootPath = this.props.match.path;
     return (
       <Switch>
-        <ConnectedRoute path={`${rootPath}`} {...this.props} component={MARCCat} id="navigation_root" />
         <ConnectedRoute path={`${rootPath}/simpleSearch`} {...this.props} component={SimpleSearch} id="simple_search" />
         <ConnectedRoute path={`${rootPath}/externalSearch`} {...this.props} component={() => {}} id="external_search" />
         <ConnectedRoute path={`${rootPath}/searchResults`} {...this.props} component={SearchResults} id="search_result" />
@@ -33,6 +44,7 @@ export default class Router extends React.Component<*> {
         <ConnectedRoute path={`${rootPath}/templatelist`} {...this.props} component={TemplateView} id="template_list" />
         <ConnectedRoute path={`${rootPath}/indexList`} {...this.props} component={IndexList} id="index_list" />
         <ConnectedRoute path={`${rootPath}/diacritic`} {...this.props} component={Diacritic} id="diacritic" />
+        <Route path={`${rootPath}`} {...this.props} render={()=> <Empty />} id="navigation_root" />
       </Switch>
     );
   }

1ecc8c5e64775181d3bf418d3bf450b7b19687e9
Alice Guercio <alice.guercio@atcult.it>
2018-08-06 09:41:08 +0200

    Revert "Revert "Revert "cleaning"""
    This reverts commit 5ad58edf25d3381b6f1a040e66a58b923880300a.

--- src/Template/form/MandatoryList.js
+++ src/Template/form/MandatoryList.js
@@ -5,6 +5,7 @@
 import React from 'react';
 import MultiColumnList from '@folio/stripes-components/lib/MultiColumnList';
 import { connect } from '@folio/stripes-connect';
+import * as C from '../../Utils/Constant';
 import { remapMultiArray } from '../../Utils/Mapper';
 
 type CreateTemplateProps = {
@@ -17,26 +18,39 @@ type CreateTemplateProps = {
 
 
 class MandatoryList extends React.Component<CreateTemplateProps, CreateTemplateState> {
-  render() {
-    const columnMapping = {
-      code: '',
-      description: '',
-      displayValue: ''
-    };
-    const obj = remapMultiArray(this.props.fields);
-    return (
-      <MultiColumnList
-        contentData={obj}
-        columnMapping={columnMapping}
-        visibleColumns={[
-          'code',
-          'description',
-          'displayValue',
-        ]}
-        ariaLabel="TemplateNewMandatory"
-      />
-    );
-  }
+    static manifest = Object.freeze({
+      records: {
+        type: C.RESOURCE_TYPE,
+        root: C.ENDPOINT.BASE_URL,
+        path: C.ENDPOINT.TEMPLATE_MANDATORY,
+        headers: C.ENDPOINT.HEADERS,
+        records: 'fields',
+        GET: {
+          params: { lang: C.ENDPOINT.DEFAULT_LANG },
+        },
+      },
+    });
+
+    render() {
+      const columnMapping = {
+        code: '',
+        description: '',
+        displayValue: ''
+      };
+      const obj = remapMultiArray(this.props.fields);
+      return (
+        <MultiColumnList
+          contentData={obj}
+          columnMapping={columnMapping}
+          visibleColumns={[
+            'code',
+            'description',
+            'displayValue',
+          ]}
+          ariaLabel="TemplateNewMandatory"
+        />
+      );
+    }
 }
 
 export default connect(MandatoryList);

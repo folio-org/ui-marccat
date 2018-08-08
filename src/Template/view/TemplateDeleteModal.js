790ad686a797dc68a175ff2b496ad39094c8aaf5
cchiama <christian.chiama@atcult.it>
2018-08-08 18:41:40 +0200

    fix props

--- src/Template/view/TemplateDeleteModal.js
+++ src/Template/view/TemplateDeleteModal.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import ConfirmationModal from '@folio/stripes-components/lib/ConfirmationModal';
 
-export default function TemplateDetailModal({ open, onDelete, hideConfirm, ...props }) {
+type TemplateDetailModalProps = {
+    open: bool;
+    stripes: Object;
+    onDelete: () => void;
+    hideConfirm: () => void;
+};
+
+export default function TemplateDetailModal({ open, onDelete, hideConfirm, ...props }: TemplateDetailModalProps) {
   const formatMsg = props.stripes.intl.formatMessage;
   const modalHeading = formatMsg({ id: 'ui-marccat.template.delete' });
   const modalMessage = formatMsg({ id: 'ui-marccat.template.delete.modal' });

8323cae003e151cd7c7397ab9b77574c3144e1cf
cchiama <christian.chiama@atcult.it>
2018-08-08 22:56:06 +0200

    change HOC syntax to better performance

--- src/Core/Provider/withLogger.js
+++ src/Core/Provider/withLogger.js
@@ -1,7 +1,5 @@
 import * as React from 'react';
 // import logger from '@folio/stripes-logger';
-import getDisplayName from '../Utils/utils';
-
 // const Logger = require('./stripes-logger.js');
 
 // const l = new Logger('redux,action');
@@ -9,19 +7,8 @@ import getDisplayName from '../Utils/utils';
 // l.log('path', `substitutePath generated ${path}`);
 // l.log('action', 'user searched for', query);
 
-export default function withLogger(WrappedComponent) {
-  class WithLogger extends React.Component {
-    render() {
-      // const { cat } = this.props;
-      // const log = new Logger(cat);
-      return (
-        <WrappedComponent
-          {...this.props}
-          // log={log}
-        />
-      );
-    }
-  }
-  WithLogger.displayName = `WithLogger(${getDisplayName(WrappedComponent)})`;
-  return WithLogger;
+export default function withLogger<Props, Component: React.ComponentType<Props>>(
+  WrappedComponent: Component
+): React.ComponentType<React.ElementConfig<Component>> {
+  return props => <WrappedComponent {...props} log={{}} />;
 }
--- src/Core/Provider/withNavigation.js
+++ src/Core/Provider/withNavigation.js
@@ -1,29 +1,7 @@
 import * as React from 'react';
 
-export const NavigationContext = React.createContext({});
-
-type Props = {};
-type State = {}
-
-function getDisplayName(WrappedComponent) {
-  return WrappedComponent.displayName || WrappedComponent.name || 'Component';
+export default function withNavigation<Props, Component: React.ComponentType<Props>>(
+  WrappedComponent: Component
+): React.ComponentType<React.ElementConfig<Component>> {
+  return props => <WrappedComponent {...props} log={{}} />;
 }
-
-function withNavigation(WrappedComponent) {
-  class WithNavigation extends React.Component<Props, State> {
-    render() {
-      return (
-        <NavigationContext.Consumer>
-          {() => <WrappedComponent
-            {...this.props}
-          />}
-        </NavigationContext.Consumer>
-      );
-    }
-  }
-  WithNavigation.displayName = `WithNavigation(${getDisplayName(WrappedComponent)})`;
-  return WithNavigation;
-}
-
-export default withNavigation;
-

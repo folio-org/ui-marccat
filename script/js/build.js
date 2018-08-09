5e604679261e303b425018b2ca11ba14ce1a8571
cchiama <christian.chiama@atcult.it>
2018-08-09 17:10:47 +0200

    test build

--- script/js/build.js
+++ script/js/build.js
@@ -9,6 +9,8 @@ const execSync = (cmd) => {
 
 const lunchBuild = () => {
   log.ok('Start Building');
+  execSync('yarn config set @folio:registry https://repository.folio.org/repository/npm-folioci/');
+  execSync('yarn global add @folio/stripes-cli');
   execSync('stripes build --output dist/ --sourcemap --languages en');
   log.ok('End Building for language en-US');
 };

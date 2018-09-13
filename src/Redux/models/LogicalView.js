ef4e2152944a78f8e0e84f97fecf489414a22acd
Matteo Pascucci <matteo.pascucci@atcult.it>
2018-09-13 10:08:15 +0200

    update for redux restart

--- src/Redux/models/LogicalView.js
+++
@@ -1,9 +0,0 @@
-
-interface KeyValue {
-    label: string;
-    value: string;
-}
-
-export default class LogicalView {
-    views: Array<KeyValue>
-}
--- src/Settings/index.js
+++
@@ -1,2 +0,0 @@
-export { default as Settings } from './components/Settings';
-export { default as GeneralSettings } from './components/GeneralSettings';

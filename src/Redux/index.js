fb5d66c08893b753f37ceae3c52b9623fa95f919
Matteo Pascucci <matteo.pascucci@atcult.it>
2018-09-14 17:12:20 +0200

    bozza search-engine

--- src/Redux/index.js
+++ src/Redux/index.js
@@ -1,10 +1,10 @@
 import { combineReducers } from 'redux';
 import { combineEpics } from 'redux-observable';
-import rootReducer from './reducers/Reducer';
+import { logicalViewReducer } from './reducers/Reducer';
 import marccatEpic from './epic/epics';
 
 export const reducer = combineReducers({
-  root: rootReducer
+  root: logicalViewReducer
 });
 
 export const epics = combineEpics(
--- src/Redux/reducers/Reducer.js
+++ src/Redux/reducers/Reducer.js
@@ -3,10 +3,23 @@ import { ActionTypes } from '../actions/Actions';
 const initialState = {
   views: undefined,
   isLoading: false,
-  error: null
+  error: null,
+  repos: [],
+  searchInputValue: ''
 };
 
-export default function rootReducer(state = initialState, action) {
+export const reducer = (state = initialState, action) => {
+  switch (action.type) {
+  case 'SEARCH_INPUT_CHANGE':
+    return Object.assign({}, state, { searchInputValue: action.value });
+  case 'SET_REPOS':
+    return Object.assign({}, state, { repos: action.repos });
+  default:
+    return state;
+  }
+};
+
+export function logicalViewReducer(state = initialState, action) {
   switch (action.type) {
   case ActionTypes.FETCH_LOGICAL_VIEWS:
     return {

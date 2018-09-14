79ba3deb100775765e01bc10e6b609be656d08e2
Matteo Pascucci <matteo.pascucci@atcult.it>
2018-09-14 11:32:43 +0200

    reducer and epic cleaning

--- src/Redux/index.js
+++ src/Redux/index.js
@@ -1,10 +1,9 @@
 import { combineReducers } from 'redux';
 import { combineEpics } from 'redux-observable';
-import { reducer as marccatReducer, rootReducer } from './reducers/Reducer';
+import rootReducer from './reducers/Reducer';
 import marccatEpic from './epic/epics';
 
 export const reducer = combineReducers({
-  data: marccatReducer,
   root: rootReducer
 });
 

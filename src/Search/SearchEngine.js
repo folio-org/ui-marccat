4fba8161150f64ab08664441e82f24ea59326bc7
Matteo Pascucci <matteo.pascucci@atcult.it>
2018-09-17 15:11:14 +0200

    add accordion and stripes-component

--- src/Search/SearchEngine.js
+++ src/Search/SearchEngine.js
@@ -1,20 +1,21 @@
 import React from 'react';
+import Button from '@folio/stripes-components/lib/Button';
 import { connect } from 'react-redux';
+import TextField from '@folio/stripes-components/lib/TextField';
 import { ENDPOINT } from '../Utils/Constant';
 
 type SearchEngineProps = {
-    inputValue: string,
-    handleInputChange: Function,
-    handleSubmit: Function,
+  inputValue: string,
+  handleInputChange: Function,
+  handleSubmit: Function,
 }
 
-function SearchEngine(props:SearchEngineProps) {
+function SearchEngine(props: SearchEngineProps) {
   return (
-    <div>
-      <form onSubmit={() => props.handleSubmit(props.inputValue)}>
-        <input value={props.inputValue} onChange={props.handleInputChange} />
-      </form>
-    </div>
+    <form>
+      <TextField value={props.inputValue} onChange={props.handleInputChange} />
+      <Button fullWidth onClick={(evt) => props.handleSubmit(props.inputValue)} />
+    </form>
   );
 }
 
@@ -30,9 +31,10 @@ const mapDispatchToProps = (dispatch) => {
     handleInputChange: (evt) => {
       dispatch({ type: 'SEARCH_INPUT_CHANGE', value: evt.target.value });
     },
-    handleSubmit: (evt, inputValue) => {
-      evt.preventDefault();
-      fetch(ENDPOINT.BASE_URL.concat('/').concat(ENDPOINT.SEARCH_URL).concat(`?lang=eng&q=${inputValue}&from=1&to=10&view=1&ml=170&dpo=1`))
+    handleSubmit: (evt, query) => {
+      fetch(ENDPOINT.BASE_URL.concat('/')
+        .concat(ENDPOINT.SEARCH_URL)
+        .concat(`?lang=eng&q=${query}&from=1&to=10&view=1&ml=170&dpo=1`))
         .then((response) => {
           return response.json();
         })

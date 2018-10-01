8bf8244bcd5ce0f5b8e936c927b548672c7db510
Christian Chiama <christian.chiama@atcult.it>
2018-10-02 00:33:01 +0200

    update
    update

--- src/Lib/index.js
+++ src/Lib/index.js
@@ -2,5 +2,6 @@ export { default as AutoComplete } from './AutoComplete';
 export { default as DotLoader } from './Loader';
 export { default as Printer } from './Printer';
 export { default as EmptyMessage } from './Message';
+export { FilterGroup, SortFilters } from './Filter';
 export { ToolbarMenu, ToolbarButtonMenu } from './Toolbar/Menu';
 export { actionMenuItem } from './Toolbar/ActionMenu';

3e0b8bed561be1585b090cd6f3e99829bca3a7e3
Matteo Pascucci <matteo.pascucci@atcult.it>
2018-10-05 12:28:08 +0200

    static result in a textarea

---
+++ src/Scan/RowDetails.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { FormattedMessage } from 'react-intl';
+import TextArea from '@folio/stripes-components/lib/TextArea';
+import Pane from '@folio/stripes-components/lib/Pane';
+import * as C from '../Utils/Constant';
+
+type Props= {
+    items:Array<any>,
+}
+
+function RowDetails(props:Props) {
+  return (
+    <Pane
+      defaultWidth="30%"
+      paneTitle={<FormattedMessage id="ui-marccat.browsing.detail" />}
+      appIcon={{ app: C.META.ICON_TITLE }}
+    >
+      <TextArea
+        value={props.items}
+      />
+    </Pane>
+  );
+}
+
+
+export default (connect(
+  ({ marccat: { details } }) => ({
+    items: details.records
+  })
+)(RowDetails));

b94f0903207088ffaedb9b5db32530d755d81fd0
Christian Chiama <christian.chiama@atcult.it>
2018-10-19 13:42:31 +0200

    update

--- src/components/Search/Filter/FiltersContainer.js
+++ src/components/Search/Filter/FiltersContainer.js
@@ -48,7 +48,7 @@ export default class FiltersContainer extends React.Component {
   }
 
   onClearFilter = () => {
-    this.setState(s => {
+    this.setState(() => {
       const filters = {};
       return { filters };
     });

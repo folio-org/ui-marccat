225970fc904209f385af80755edfeac21eacdf27
Christian Chiama <christian.chiama@atcult.it>
2018-10-29 15:01:54 +0100

    fix
    fix

--- src/components/Lib/index.js
+++
@@ -1,3 +0,0 @@
-export { default as EmptyMessage } from './Message';
-export { ToolbarMenu, ToolbarButtonMenu } from './Toolbar/Menu';
-export { actionMenuItem } from './Toolbar/ActionMenu';
--- src/core/index.js
+++
@@ -1,4 +0,0 @@
-
-export { default as injectCommonProp } from './Provider/withInjection';
-export { default as Props } from './type/props';
-export { default as registerKeybordListener } from './Keyboard/KeyHOC';

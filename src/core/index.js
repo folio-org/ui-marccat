3a557cf4ab23ef0028cebd13e42b6f42cb08744b
Christian Chiama <christian.chiama@atcult.it>
2018-10-31 14:19:15 +0100

    fix
    fix

--- src/core/index.js
+++ src/core/index.js
@@ -1,4 +1,3 @@
 
 export { default as injectCommonProp } from './Provider/withInjection';
 export { default as Props } from './type/props';
-export { default as registerKeybordListener } from './Keyboard/KeyHOC';

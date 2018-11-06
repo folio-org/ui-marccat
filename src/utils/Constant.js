747321b7900b0da4eb20c1ab9cfbddc7ba187a46
Matteo Pascucci <matteo.pascucci@atcult.it>
2018-11-06 12:29:38 +0100

    fix details panels

--- src/utils/Constant.js
+++ src/utils/Constant.js
@@ -15,7 +15,7 @@ export const ENDPOINT = {
     'Content-Type': 'application/json',
     'x-okapi-tenant': 'tnx',
   },
-  BASE_URL: 'http://localhost:8080/cataloging',
+  BASE_URL: 'marccat',
   SEARCH_URL: '/searchVertical',
   SEARCH_URL_JSON: '/search',
   DOC_COUNT_URL: 'document-count-by-id'

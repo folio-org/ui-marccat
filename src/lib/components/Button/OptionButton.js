bcbbb5801f7a5339016f0d9eff6cb97ec07e7339
Christian Chiama <christian.chiama@atcult.it>
2018-12-28 00:12:05 +0100

    fix checkbox redux-form css
    fix checkbox redux-form css

--- src/lib/components/Button/OptionButton.js
+++ src/lib/components/Button/OptionButton.js
@@ -23,27 +23,55 @@ export function RadioIconButton({ ...props }:Props) {
 }
 
 
-function CheckboxIconButton({ ...props }:Props) {
-  const { labels } = props;
-  return (
-    <form name="checkboxForm">
-      { labels.map((l, i) => (
-        <div key={i}>
-          <Field
-            id={`checkbox-${l}`}
-            className="checkbox"
-            name={`checkbox-${l}`}
-            type="checkbox"
-            component="input"
-          />
-          <label htmlFor={`checkbox-${i}`} className="checkbox">{l}</label>
-        </div>
-      ))
-      }
-    </form>
-  );
-}
+class CheckboxIconButton extends React.Component<Props, {}> {
+  constructor(props:Props) {
+    super(props);
 
+    this.state = {
+      isSelected: false
+    };
+
+    this.handleCheckboxValue = this.handleCheckboxValue.bind(this);
+  }
+
+
+  handleCheckboxValue = e => {
+    const { dispatch, change } = this.props;
+    const { isSelected } = this.state;
+    dispatch(change(`${e.target.htmlFor}`, !isSelected));
+    this.setState({
+      isSelected: !isSelected
+    });
+  };
+
+  render() {
+    const { labels } = this.props;
+    return (
+      <form name="checkboxForm">
+        { labels.map((l, i) => (
+          <div key={i}>
+            <Field
+              id={`checkbox-${l}`}
+              className="checkbox"
+              name={`checkbox-${l}`}
+              type="checkbox"
+              component="input"
+              checked={`checkbox-${l}`}
+            />
+            <label
+              htmlFor={`checkbox-${l}`}
+              className="checkbox"
+              onClick={this.handleCheckboxValue}
+            >
+              {l}
+            </label>
+          </div>
+        ))
+        }
+      </form>
+    );
+  }
+}
 export default reduxForm({
   form: 'checkboxForm',
   destroyOnUnmount: false

8a85ed7540b30436f8e9fb32727c39583820459e
minkimcello@gmail.com <minkimcello@gmail.com>
2020-07-28 10:05:27 -0400

    Comment out code

--- src/components/Search/Button/EditRecord.js
+++ src/components/Search/Button/EditRecord.js
@@ -4,26 +4,26 @@ import * as React from 'react';
 import { Button, Icon } from '@folio/stripes/components';
 import { connect } from 'react-redux';
 import { Localize, findParam } from '../../../shared';
-import { lockRecordAction } from '../../Cataloguing/Actions';
+// import { lockRecordAction } from '../../Cataloguing/Actions';
 
 const EditRecordButton = ({ ...props }) => {
-  const lockRecord = id => {
-    const { dispatch, store } = props;
-    const user = store.getState().okapi.currentUser.username;
-    const idLock = store.getState().okapi.currentUser.id;
-    console.log(idLock);
-    const payload = {
-      user,
-      id,
-      idLock,
-    };
-    dispatch(lockRecordAction(payload));
-  };
+  // const lockRecord = id => {
+  //   const { dispatch, store } = props;
+  //   const user = store.getState().okapi.currentUser.username;
+  //   const idLock = store.getState().okapi.currentUser.id;
+  //   console.log(idLock);
+  //   const payload = {
+  //     user,
+  //     id,
+  //     idLock,
+  //   };
+  //   dispatch(lockRecordAction(payload));
+  // };
   const handleClickEdit = () => {
     const { router, toggleFilterPane } = props;
     const id = findParam('id');
     toggleFilterPane();
-    lockRecord(id);
+    // lockRecord(id);
     router.push(`/marccat/cataloging?id=${id}&mode=edit`);
   };
 

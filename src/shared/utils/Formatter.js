c212200fbbbd352a56b94a99e40fed07e851c7a6
apineiroR <apineiro@ibaiscanbit.com>
2020-11-20 12:02:31 +0100

    Button View in inventory not visible for Authorities. Code cleared

--- src/shared/utils/Formatter.js
+++ src/shared/utils/Formatter.js
@@ -217,32 +217,6 @@ export const resultsFormatterForAssociated = {
     subjectTags(x)
   ),
 };
-export const authDetailsResultFormatter = () => {
-  const authDetails = {
-    resultView: x => (<AppIcon size="small" app="marccat" iconKey="marc-bib" />),
-  };
-  return authDetails;
-};
-
-export const authDetailsColumnMapper = () => {
-  const authDetailsMapper = {
-    'resultView': '',
-    'sampleId': '',
-    'username': '',
-    'surname': '',
-  };
-  return authDetailsMapper;
-};
-
-export const authDetailsRenderColumn = () => {
-  const authDetailsVisibleCol = [
-    'resultView',
-    'sampleId',
-    'username',
-    'surname',
-  ];
-  return authDetailsVisibleCol;
-};
 
 export const resultsFormatter = (
   isBibsOnly: ?boolean = true,

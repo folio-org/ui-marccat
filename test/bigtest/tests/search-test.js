5fea341364b6420f8a5eb5d4cdf5810904cda04c
apineiroR <apineiro@ibaiscanbit.com>
2020-12-09 11:46:07 +0100

    Test added

--- test/bigtest/tests/search-test.js
+++ test/bigtest/tests/search-test.js
@@ -107,6 +107,22 @@ describe('Search', () => {
     });
   });
 
+  describe('should test Local Id number searches', () => {
+    beforeEach(async function () {
+      await searchInteractor.selectIndexes.selectOption('Local id.Number (001)');
+      await searchInteractor.selectCondition.selectOption('Contains');
+      await searchInteractor.searchTextArea.fill('1');
+      await searchInteractor.buttonSearch.click();
+      await searchInteractor.selectIndexes.selectOption('Local id.Number (001)');
+      await searchInteractor.selectCondition.selectOption('Contains');
+      await searchInteractor.searchTextArea.fill('test');
+      await searchInteractor.buttonSearch.click();
+    });
+    
+    it('Control number should find results', () => {
+      expect(searchInteractor.countResults).to.be.greaterThan(1);
+    });
+  });
   
   describe('should test authority control number searches', () => {
     beforeEach(async function () {
